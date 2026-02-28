61d50a5d1190c57aa56f16a9e90c6fb8f8043044
Kaan <59264452+r4gast@users.noreply.github.com>
2026-02-28 02:30:00 +0100

    Update falconpy.py

--- scripts/falconpy.py
+++ scripts/falconpy.py
@@ -8,6 +8,7 @@ print(client_id)
 print(client_secret)
 
 
+'''
 auth = OAuth2(
     client_id = client_id,
     cliend_secret = client_secret
@@ -15,6 +16,6 @@ auth = OAuth2(
 
 
 print("Authenticated", auth.token_status)
-
+'''
 
 print("falconpy executed")
