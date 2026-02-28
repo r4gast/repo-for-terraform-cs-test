0826102c6810622165ea3b746bc3f58a1e36a160
Kaan <59264452+r4gast@users.noreply.github.com>
2026-02-28 02:23:09 +0100

    Update falconpy.py

--- scripts/falconpy.py
+++ scripts/falconpy.py
@@ -1,1 +1,20 @@
+import os
+from falconpy import OAuth2
+
+client_id = os.getenv("FALCON_CLIENT_ID")
+client_secret = os.getenv("FALCON_CLIENT_SECRET")
+
+print(client_id)
+print(client_secret)
+
+
+auth = OAuth2(
+    client_id = client_id,
+    cliend_secret = client_secret
+)
+
+
+print("Authenticated", auth.token_status)
+
+
 print("falconpy executed")
