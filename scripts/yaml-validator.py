75a5327a849c7b017b18960431b3a2690848ebd6
Kaan <59264452+r4gast@users.noreply.github.com>
2026-03-02 15:38:54 +0100

    Create yaml-validator.py

---
+++ scripts/yaml-validator.py
@@ -0,0 +1,48 @@
+import sys
+import yaml
+import glob
+
+REQUIRED_FIELDS = [
+  "id",
+  "name",
+  "severity",
+  "owner",
+  "rule" ]
+
+def validate_file(file_path):
+  print(f"Validating {file_path}...")
+
+  with open(file_path, "r") as f:
+    try:
+      data = yaml.safe_load(f)
+    except yaml.YAMLError as e:
+      print(f"YAML syntax error in {file_path}")
+      print(e)
+      sys.exit(1)
+
+  for field in REQUIRED_FIELDS:
+    if field not in data:
+      print(f"Missing required field '{field} in {field_path}")
+      sys.exit(1)
+
+  if "query" not in data["rule"]:
+    print(f"Missing 'rule.query' in {file_path}")
+    sys.exit(1)
+
+  print(f"{file_path} is valid.\n")
+
+
+if __name__ == "__main__":
+  files =  glob.glob("detections/*.yaml")
+
+  if not files:
+    print("No detection files found")
+    sys.exit(0)
+
+  for file in files:
+    validate_file(file)
+
+  print("All Detection YAML files validated successfuly")
+
+  
+    
