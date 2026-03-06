b91dc496f0c4020fb5d071a9a6c8c8f8297405aa
Kaan <59264452+r4gast@users.noreply.github.com>
2026-03-06 10:47:56 +0100

    Update yaml-validator.py

--- scripts/yaml-validator.py
+++ scripts/yaml-validator.py
@@ -21,7 +21,7 @@ def validate_file(file_path):
       sys.exit(1)
 
   print(type(data))
-  print(data[0]["mitre"]["tactic"]
+  print(data[0]["mitre"]["tactic"])
   for field in REQUIRED_FIELDS:
     if field not in data:
       print(f"Missing required field '{field} in {field_path}")
